6d4f9647e19141ea0de4a39f06ea21aa02a61efc
Moltclaw AI <setrox@moltclaw.local>
2026-04-28 12:46:40 +0300

    wip: add cleanup to test setup

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,13 @@
 import '@testing-library/jest-dom';
+import { cleanup } from '@testing-library/react';
+import { beforeEach } from 'vitest';
+beforeEach(() => {
+  cleanup();
+});
+import { cleanup } from '@testing-library/react';
+import { beforeEach } from 'vitest';
+
+beforeEach(() => {
+  cleanup();
+});
+
